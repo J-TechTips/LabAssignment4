e0909889a7a5807f9c3f34fcdbb618c3074b5f2b
J-TechTips <jtecktips@gmail.com>
2021-12-20 01:29:47 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ArtworkComponent } from './artwork/artwork.component';
+import { ArtistComponent } from './artist/artist.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { routerModule } from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ArtworkComponent,
+    ArtistComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
